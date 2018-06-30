f9b8e90c08e190f25fdfa97b6c8131dd2d1805a6
blackbeam <aikorsky@gmail.com>
2018-06-30 14:27:40 +0300

    Clarify MySql versions, supported by crypto::encrypt function

--- src/crypto/mod.rs
+++ src/crypto/mod.rs
@@ -2,6 +2,8 @@ pub mod der;
 pub mod rsa;
 
 /// Helper function to encrypt mysql password using a public key loaded from a server.
+///
+/// It will use OAEP padding, so MySql versions prior to 8.0.5 are not supported.
 pub fn encrypt(pass: &[u8], key: &[u8]) -> Vec<u8> {
     let pub_key = self::rsa::PublicKey::from_pem(key);
     let rng = ::rand::OsRng::new().unwrap();
