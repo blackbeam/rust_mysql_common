a0a14d99d75354f039abf890fd34bbdeb36ba2ee
Anatoly Ikorsky <aikorsky@gmail.com>
2020-09-01 14:38:44 +0300

    Remove unnecessary import

--- src/proto/codec/error.rs
+++ src/proto/codec/error.rs
@@ -6,7 +6,6 @@
 // option. All files in the project carrying such notice may not be copied,
 // modified, or distributed except according to those terms.
 
-use bitflags::_core::fmt::Formatter;
 use std::fmt;
 use std::io;
 
